9dbb6dcd44ca9f5ce7b40bc4ce6c4b93ddd10533
Steve Kukla <stevenjkukla@gmail.com>
2020-09-24 16:17:28 -0700

    Added checks for demo shell module to allow for data packs as a custom module

--- app/cookbooks/magento_demo_builder/recipes/default.rb
+++ app/cookbooks/magento_demo_builder/recipes/default.rb
@@ -5,6 +5,9 @@
 # Copyright:: 2020, Steve Kukla, All Rights Reserved.
 sample_data_flag = node[:magento_demo_builder][:magento][:build][:sample_data]
 build_action = node[:magento_demo_builder][:magento][:build][:action]
+web_root = node[:magento_demo_builder][:init][:web_root]
+demo_shell_path = node[:magento_demo_builder][:demo_shell][:directory]
+fixtures_directory = node[:magento_demo_builder][:demo_shell][:fixtures_directory]
 
 magento_config "Create drop directories" do
     action [:create_samba_drops, :create_non_samba_drops]
@@ -13,20 +16,26 @@ end
 
 magento_demo_builder "Remove existing data from the database" do
     action :remove_data_patches
-    only_if { build_action == "update" }
+    only_if { 
+        build_action == "update" &&
+        Dir.exist?("#{web_root}/#{demo_shell_path}/#{fixtures_directory}") 
+    }
 end
 
 magento_demo_builder "Remove existing custom demo media" do
     action :remove_media
+    only_if { Dir.exist?("#{web_root}/#{demo_shell_path}/#{fixtures_directory}") }
     not_if { sample_data_flag }
 end
 
 magento_demo_builder "Install custom demo data files" do
     action [:remove_data, :install_data]
+    only_if { Dir.exist?("#{web_root}/#{demo_shell_path}/#{fixtures_directory}") }
 end
 
 magento_demo_builder "Copy custom demo media" do
     action :add_media
+    only_if { Dir.exist?("#{web_root}/#{demo_shell_path}/#{fixtures_directory}") }
 end
 
 magento_app "Set permissions on media directories and files" do
