675d2e88b90eb29e17746b82237e3c48b3902c24
Steve Kukla <stevenjkukla@gmail.com>
2020-11-30 00:52:25 -0500

    Added main_app class and refactored Vagrantfile

---
+++ app/lib/main_app.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require 'json'
+
+# Application class
+class App
+  attr_accessor :dirs, :files, :paths, :entries, :colors, :settings
+
+  def initialize
+    @dirs = {
+      root: File.expand_path('..', Dir.pwd).to_s,
+      app: Pathname.new("#{File.expand_path('..', Dir.pwd)}/app"),
+      environments: 'environments',
+      workspace: 'demo',
+      backup: 'backup',
+      data_packs: 'data_packs',
+      patches: 'patches',
+      certificate: 'certificate'
+    }
+    @files = {
+      environment: 'vm.rb',
+      config: 'config.json'
+    }
+    @paths = {
+      backups: Pathname.new("#{@dirs[:root]}/#{@dirs[:workspace]}/#{@dirs[:backup]}"),
+      data_packs: Pathname.new("#{@dirs[:root]}/#{@dirs[:workspace]}/#{@dirs[:data_packs]}"),
+      patches: Pathname.new("#{@dirs[:root]}/#{@dirs[:workspace]}/#{@dirs[:patches]}"),
+      chef_backup_files: Pathname.new("#{@dirs[:app]}/cookbooks/magento_restore/files/default/"),
+      chef_data_pack_files: Pathname.new("#{@dirs[:app]}/cookbooks/magento_demo_builder/files/default/"),
+      chef_patch_files: Pathname.new("#{@dirs[:app]}/cookbooks/magento_patches/files/default/"),
+      ssl_certificates: Pathname.new("#{@dirs[:app]}/#{@dirs[:certificate]}"),
+      environment_file: Pathname.new("#{@dirs[:app]}/#{@dirs[:environments]}/#{@files[:environment]}"),
+      config_file: Pathname.new("#{@dirs[:root]}/#{@files[:config]}")
+    }
+    @entries = {
+      user_backups: Dir.entries(@paths[:backups]) - %w[. .. .gitignore],
+      user_data_packs: Dir.entries(@paths[:data_packs]) - %w[. .. .gitignore],
+      user_patches: Dir.entries(@paths[:patches]) - %w[. .. .gitignore],
+      chef_data_pack_files: Dir.entries(@paths[:chef_data_pack_files]) - %w[. .. .gitignore],
+      chef_backup_files: Dir.entries(@paths[:chef_backup_files]) - %w[. .. .gitignore],
+      chef_patch_files: Dir.entries(@paths[:chef_patch_files]) - %w[. .. .gitignore]
+    }
+    @colors = {
+      bold: `tput bold`,
+      reg: `tput sgr0`,
+      green: `tput setaf 2`,
+      magenta: `tput setaf 5`,
+      cyan: `tput setaf 6`
+    }
+    @settings = JSON.parse(File.read(@paths[:config_file]))
+  end
+
+  def check_for_plugins
+    completed = []
+    plugins = @settings['vagrant']['plugins']['all']
+    if @settings['vm']['hypervisor'] == 'virtualbox'
+      plugins.push(*@settings['vagrant']['plugins']['virtualbox'])
+    else
+      plugins.push(*@settings['vagrant']['plugins']['vmware'])
+    end
+    plugins.each do |plugin|
+      unless Vagrant.has_plugin?(plugin.to_s)
+        system("vagrant plugin install #{plugin}", chdir: '/tmp') || exit!
+        completed << plugin
+      end
+    end
+    unless plugins.difference(completed).any?
+      abort("#{@colors[:green]}[SUCCESS]: #{@colors[:reg]}Plugins have been installed. Please run the #{@colors[:bold]}#{@colors[:cyan]}vagrant up #{@colors[:reg]}command again to continue.\n")
+    end
+  end
+
+  def check_for_data_pack_errors
+    if @settings['custom_demo'].key?('data_packs') && (!@settings['custom_demo']['data_packs'].nil? || !@settings['custom_demo']['data_packs'].empty?)
+      @settings['custom_demo']['data_packs'].each do |_key, data_pack|
+        if data_pack['name'].nil? || data_pack['name'].empty?
+          abort("#{@colors[:magenta]}[OOPS]: #{@colors[:reg]}It looks like you're missing one of your data pack names in your config.json file.\n\n")
+        end
+        if data_pack['repository_url'].nil? || data_pack['repository_url'].empty?
+          abort("#{@colors[:magenta]}[OOPS]: #{@colors[:reg]}It looks like you're missing one of your data pack repository urls in your config.json file.\n\n")
+        end
+      end
+      missing_data_packs = ((@settings['custom_demo']['data_packs'].map { |_key, value| value['repository_url'] }) - @entries[:user_data_packs])
+      unless missing_data_packs.nil? || missing_data_packs.empty?
+        abort("#{@colors[:magenta]}[OOPS]: #{@colors[:reg]}Make sure the following folders are in your demo workspace and properly configured in config.json: #{@colors[:bold]}#{@colors[:cyan]}#{missing_data_packs.join(', ')}\n\n")
+      end
+    end
+  end
+
+  def copy_data_packs
+    unless @entries[:chef_data_pack_files].empty?
+      @entries[:chef_data_pack_files].each do |entry|
+        FileUtils.rm_r("#{@paths[:chef_data_pack_files]}/#{entry}")
+      end
+    end
+    @entries[:user_data_packs].each do |entry|
+      FileUtils.cp_r("#{@paths[:data_packs]}/#{entry}", @paths[:chef_data_pack_files])
+    end
+  end
+
+  def copy_patches
+    unless @entries[:chef_patch_files].empty?
+      @entries[:chef_patch_files].each do |entry|
+        FileUtils.rm_r("#{@paths[:chef_patch_files]}/#{entry}")
+      end
+    end
+    @entries[:user_patches].each do |entry|
+      FileUtils.cp_r("#{@paths[:patches]}/#{entry}", @paths[:chef_patch_files])
+    end
+  end
+
+  def check_for_backup_errors
+    if !@settings['custom_demo'].key?('backup') || (@settings['custom_demo'].key?('backup') && @settings['custom_demo']['backup'].empty?)
+      abort("#{@colors[:magenta]}[OOPS]: #{@colors[:reg]}You have a build action of #{@colors[:bold]}#{@colors[:cyan]}restore#{@colors[:reg]}, but you haven't added any local backup files or specified a remote backup to download. You silly goose, you.\n\n")
+    end
+  end
+
+  def copy_local_backup_files
+    unless @entries[:chef_backup_files].empty?
+      @entries[:chef_backup_files].each do |entry|
+        FileUtils.rm_r("#{@paths[:chef_backup_files]}/#{entry}")
+      end
+    end
+    zip_file = @entries[:user_backups].find { |file| file.include?('.zip') }
+    if zip_file
+      FileUtils.cp_r("#{@paths[:backups]}/#{zip_file}", @paths[:chef_backup_files])
+    else
+      @entries[:user_backups].each do |entry|
+        ['.tgz', '.sql'].any? do |extension|
+          FileUtils.cp_r("#{@paths[:backups]}/#{entry}", @paths[:chef_backup_files]) if entry.include?(extension)
+        end
+      end
+    end
+  end
+
+  def create_environment_file
+    environment_file_content = [
+      'name "vm"',
+      'description "Configuration file for the Kukla Demo VM"',
+      "default_attributes(#{@settings})"
+    ]
+    File.open(@paths[:environment_file], 'w+') do |file|
+      file.puts(environment_file_content)
+    end
+  end
+
+  def define_urls
+    result = {}
+    demo_urls = []
+    @settings['custom_demo']['structure'].each do |_scope, scope_hash|
+      scope_hash.each do |code, url|
+        case code
+        when 'base'
+          result[:hostname] = @settings['custom_demo']['structure']['website'][code]
+        else
+          demo_urls << url
+        end
+      end
+      result[:demo_urls] = demo_urls
+    end
+    result
+  end
+
+  def clean_up_ssl_certificates
+    system("sudo security find-certificate -c #{@settings['custom_demo']['structure']['website']['base'].to_s.chomp} > /dev/null 2>&1")
+    if $?.exitstatus == 0
+      system("sudo security delete-certificate -c #{@settings['custom_demo']['structure']['website']['base'].to_s.chomp} /Library/Keychains/System.keychain")
+    end
+  end
+
+  def remove_local_ssl_certificates
+    if File.exist?("#{@paths[:ssl_certificates]}/#{@settings['custom_demo']['structure']['website']['base']}.crt")
+      FileUtils.rm_rf("#{@paths[:ssl_certificates]}/#{@settings['custom_demo']['structure']['website']['base']}.crt")
+    end
+  end
+
+  def set_up_ssl_certificates
+    system("sudo security find-certificate -c #{@settings['custom_demo']['structure']['website']['base'].to_s.chomp} > /dev/null 2>&1")
+    if $?.exitstatus == 0
+      system("sudo security delete-certificate -c #{@settings['custom_demo']['structure']['website']['base'].to_s.chomp} /Library/Keychains/System.keychain")
+    end
+    system("sudo security add-trusted-cert -d -r trustAsRoot -k /Library/Keychains/System.keychain '#{@paths[:ssl_certificates]}/#{@settings['custom_demo']['structure']['website']['base']}.crt'")
+  end
+end
