9dc626bb1f07d872eabdc02c14400644ea6f3e84
Steve Kukla <stevenjkukla@gmail.com>
2020-12-01 01:24:47 -0500

    Fixed issue - allow patches to be set to false

--- app/cookbooks/magento_patches/attributes/override.rb
+++ app/cookbooks/magento_patches/attributes/override.rb
@@ -6,11 +6,9 @@
 supported_settings = %i[apply repository_url repository_directory branch codebase_directory patches_file]
 
 supported_settings.each do |setting|
-  if node[:application][:build][:patches].is_a? Chef::Node::ImmutableMash
-    unless node[:application][:build][:patches][setting].nil?
-      override[:magento_patches][setting] = node[:application][:build][:patches][setting]
-    end
-  elsif (setting == :apply) && (node[:application][:build][:patches].is_a? TrueClass) || (node[:application][:build][:patches].is_a? FalseClass)
+  if (setting == :apply) && (node[:application][:build][:patches].is_a?(TrueClass) || node[:application][:build][:patches].is_a?(FalseClass))
     override[:magento_patches][setting] = node[:application][:build][:patches]
+  elsif node[:application][:build][:patches].is_a?(Chef::Node::ImmutableMash) && node[:application][:build][:patches][setting].nil?
+    override[:magento_patches][setting] = node[:application][:build][:patches][setting]
   end
 end
