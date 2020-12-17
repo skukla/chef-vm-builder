5c693ea8914b049a2ed42c4dcf10ae61f679bb17
Steve Kukla <stevenjkukla@gmail.com>
2020-12-16 21:20:24 -0500

    Added more intelligent URL check

--- app/cookbooks/helpers/libraries/database_helper.rb
+++ app/cookbooks/helpers/libraries/database_helper.rb
@@ -7,7 +7,7 @@ module DatabaseHelper
   def self.execute_query(db_user, db_password, db_name, query)
     connection_string = "mysql --user=#{db_user} --password=#{db_password} --database=#{db_name}"
     query_string = "\"#{query};\""
-    `#{[connection_string, query_string].join(' -e ')}`
+    `#{[connection_string, query_string].join(' -N -s -e ')}`
   end
 
   def self.patch_exists(patch_class, db_user, db_password, db_name)
@@ -22,5 +22,15 @@ module DatabaseHelper
     query_string = "\"DELETE FROM patch_list WHERE patch_name = '#{patch_class}';\""
     `#{[connection_string, query_string].join(' -e ')}`
     print "Ran #{query_string}"
+  end
+
+  def self.check_code_exists(db_user, db_password, db_name, code)
+    website_result = execute_query(db_user, db_password, db_name, "SELECT COUNT\(*\) FROM store_website WHERE code = '#{code}'")
+    store_view_result = execute_query(db_user, db_password, db_name, "SELECT COUNT\(*\) FROM store WHERE code = '#{code}'")
+    if website_result.to_i.positive? || store_view_result.to_i.positive?
+      true
+    else
+      false
+    end
   end
 end
--- app/cookbooks/magento/recipes/wrap_up.rb
+++ app/cookbooks/magento/recipes/wrap_up.rb
@@ -14,7 +14,42 @@ commands = node[:magento][:build][:hooks][:commands]
 media_gallery_commands = ['config:set system/media_gallery/enabled 1', 'media-gallery:sync']
 vm_cli_commands = commands.reject { |command| command.include?(':') }
 magento_cli_commands = commands.select { |command| command.include?(':') }
-data_pack_list = node[:magento][:data_packs]
+db_user = node[:magento][:mysql][:db_user]
+db_password = node[:magento][:mysql][:db_password]
+db_name = node[:magento][:mysql][:db_name]
+
+DemoStructureHelper.get_vhost_data(demo_structure).each do |vhost|
+  next if (vhost[:scope] == 'website' && vhost[:code] == 'base') ||
+          (vhost[:scope] == 'store_view' && vhost[:code] == 'default')
+
+  scope_value = vhost[:scope] == 'store' ? 'store view' : vhost[:scope]
+
+  magento_cli "Configuring additional unsecure URL for the #{vhost[:code]} #{scope_value}" do
+    action :config_set
+    config_path 'web/unsecure/base_url'
+    config_value "http://#{vhost[:url]}/"
+    config_scope vhost[:scope]
+    config_scope_code vhost[:code]
+    not_if { use_secure_frontend == 1 || use_secure_admin == 1 }
+    only_if { DatabaseHelper.check_code_exists(db_user, db_password, db_name, vhost[:code]) }
+  end
+  magento_cli "Configuring additional secure URL for the #{vhost[:code]} #{scope_value}" do
+    action :config_set
+    config_path 'web/secure/base_url'
+    config_value "https://#{vhost[:url]}/"
+    config_scope vhost[:scope]
+    config_scope_code vhost[:code]
+    only_if do
+      use_secure_frontend == 1 || use_secure_admin == 1 &&
+        DatabaseHelper.check_code_exists(db_user, db_password, db_name, vhost[:code])
+    end
+  end
+end
+
+mysql 'Configure default store' do
+  action :run_query
+  db_query "UPDATE store_website SET default_group_id = '1' WHERE code = 'base'"
+end
 
 magento_cli 'Running the enable_media_gallery hook' do
   action :run
@@ -46,33 +81,4 @@ vm_cli 'Running the warm cache hook' do
   command_list 'warm-cache'
   only_if { warm_cache }
   not_if { ::File.exist?("#{web_root}/var/.first-run-state.flag") && build_action == 'install' }
-end
-
-unless data_pack_list.empty?
-  DemoStructureHelper.get_vhost_data(demo_structure).each do |vhost|
-    next if (vhost[:scope] == 'website' && vhost[:code] == 'base') ||
-            (vhost[:scope] == 'store_view' && vhost[:code] == 'default')
-
-    magento_cli 'Configure additional unsecure URLs' do
-      action :config_set
-      config_path 'web/unsecure/base_url'
-      config_value "http://#{vhost[:url]}/"
-      config_scope vhost[:scope]
-      config_scope_code vhost[:code]
-      not_if { use_secure_frontend == 1 || use_secure_admin == 1 }
-    end
-    magento_cli 'Configure additional secure URLs' do
-      action :config_set
-      config_path 'web/secure/base_url'
-      config_value "https://#{vhost[:url]}/"
-      config_scope vhost[:scope]
-      config_scope_code vhost[:code]
-      only_if { use_secure_frontend == 1 || use_secure_admin == 1 }
-    end
-  end
-end
-
-mysql 'Configure default store' do
-  action :run_query
-  db_query "UPDATE store_website SET default_group_id = '1' WHERE code = 'base'"
 end
