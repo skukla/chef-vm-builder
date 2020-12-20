c3da0269cd88eef4097721af5419829ba1226951
Steve Kukla <stevenjkukla@gmail.com>
2020-12-20 00:11:03 -0500

    Updated database helper to support custom database details

--- app/cookbooks/helpers/libraries/database_helper.rb
+++ app/cookbooks/helpers/libraries/database_helper.rb
@@ -5,9 +5,26 @@
 # Copyright:: 2020, Steve Kukla, All Rights Reserved.
 module DatabaseHelper
   def self.execute_query(query)
-    @db_user = Chef.node.default['mysql']['db_user']
-    @db_password = Chef.node.default['mysql']['db_password']
-    @db_name = Chef.node.default['mysql']['db_name']
+    @db_user = if Chef.node.override['mysql']['db_user'].nil? ||
+                  Chef.node.override['mysql']['db_user'].empty?
+                 Chef.node.default['mysql']['db_user']
+               else
+                 Chef.node.override['mysql']['db_user']
+               end
+
+    @db_password = if Chef.node.override['mysql']['db_password'].nil? ||
+                      Chef.node.override['mysql']['db_password'].empty?
+                     Chef.node.default['mysql']['db_password']
+                   else
+                     Chef.node.override['mysql']['db_password']
+                   end
+
+    @db_name = if Chef.node.override['mysql']['db_name'].nil? ||
+                  Chef.node.override['mysql']['db_name'].empty?
+                 Chef.node.default['mysql']['db_name']
+               else
+                 Chef.node.override['mysql']['db_name']
+               end
 
     connection_string = "mysql --user=#{@db_user} --password=#{@db_password} --database=#{@db_name}"
     query_string = "\"#{query};\""
@@ -28,6 +45,6 @@ module DatabaseHelper
     website_result = execute_query("SELECT COUNT\(*\) FROM store_website WHERE code = '#{code}'")
     store_view_result = execute_query("SELECT COUNT\(*\) FROM store WHERE code = '#{code}'")
 
-    website_result.to_i.positive? || store_view_result.to_i.positive? ? true : false
+    website_result.to_i.positive? || store_view_result.to_i.positive?
   end
 end
