04969db7544ac0af32c275cfec5f074ba5a34d9a
Steve Kukla <stevenjkukla@gmail.com>
2021-02-27 13:40:36 -0500

    Added tree to the OS packages list

--- app/cookbooks/init/attributes/default.rb
+++ app/cookbooks/init/attributes/default.rb
@@ -6,6 +6,6 @@
 default[:init][:os][:user] = 'vagrant'
 default[:init][:os][:update] = false
 default[:init][:os][:timezone] = 'America/Los_Angeles'
-default[:init][:os][:install_package_list] = %w[zip silversearcher-ag figlet unattended-upgrades zlibc]
+default[:init][:os][:install_package_list] = %w[zip silversearcher-ag figlet unattended-upgrades zlibc tree]
 default[:init][:webserver][:type] = 'nginx'
 default[:init][:webserver][:web_root] = '/var/www/magento'
