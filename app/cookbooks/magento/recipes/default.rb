8cc96bb4a0ab4db22a6cd1f939db2939b6b2835f
Steve Kukla <stevenjkukla@gmail.com>
2021-03-27 17:32:31 -0400

    Added post_install and wrap_up to relevant build actions

--- app/cookbooks/magento/recipes/default.rb
+++ app/cookbooks/magento/recipes/default.rb
@@ -9,11 +9,12 @@ case build_action
 when 'restore'
   include_recipe 'magento::uninstall'
   include_recipe 'magento_restore::default'
+  include_recipe 'magento::post_install'
+  include_recipe 'magento::wrap_up'
 when 'reinstall'
   include_recipe 'magento::install'
-  magento_app 'Reindex and clear cache' do
-    action %i[set_application_mode reset_indexers reindex clean_cache]
-  end
+  include_recipe 'magento::post_install'
+  include_recipe 'magento::wrap_up'
 when 'refresh'
   include_recipe 'magento_demo_builder::update'
   include_recipe 'magento_demo_builder::build'
@@ -27,9 +28,8 @@ when 'update'
   include_recipe 'magento::install'
   include_recipe 'magento_demo_builder::build'
   include_recipe 'magento_demo_builder::install'
-  magento_app 'Process upgrade' do
-    action %i[set_application_mode enable_cron reset_indexers reindex clean_cache]
-  end
+  include_recipe 'magento::post_install'
+  include_recipe 'magento::wrap_up'
 when 'install'
   include_recipe 'magento::create_project'
   include_recipe 'magento::add_modules'
