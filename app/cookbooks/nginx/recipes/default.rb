ec124ac663616417e7a737570ecbd2c804bd6566
Steve Kukla <stevenjkukla@gmail.com>
2021-06-29 00:45:11 -0400

    Fixed nginx multisite issues after refactor

--- app/cookbooks/nginx/recipes/default.rb
+++ app/cookbooks/nginx/recipes/default.rb
@@ -3,26 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2020, Steve Kukla, All Rights Reserved.
-webserver_type = node[:nginx][:init][:webserver_type]
+nginx 'Install Nginx' do
+  action :install
+end
 
-if webserver_type == 'nginx'
-  nginx 'Install Nginx' do
-    action :install
-  end
+nginx 'Create web root and clear existing sites' do
+  action %i[create_web_root clear_sites]
+end
 
-  nginx 'Create web root and clear existing sites' do
-    action %i[create_web_root clear_sites]
-  end
+nginx 'Configure Nginx and enable multisite operation' do
+  action %i[configure_nginx configure_multisite enable_multisite]
+end
 
-  nginx 'Configure Nginx and enable multisite operation' do
-    action %i[configure_nginx configure_multisite enable_multisite]
-  end
+nginx 'Change user and group, enable, and restart Nginx' do
+  action %i[set_permissions enable restart]
+end
 
-  nginx 'Change user and group, enable, and restart Nginx' do
-    action %i[set_permissions enable restart]
-  end
-
-  php 'Restart PHP' do
-    action :restart
-  end
+php 'Restart PHP' do
+  action :restart
 end
