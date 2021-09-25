155b06a0179e250d39cce616227103732cfdcb17
Steve Kukla <stevenjkukla@gmail.com>
2021-09-24 22:35:56 -0400

    Added di-compile after data pack installation on refresh

--- app/vm/cookbooks/magento_demo_builder/recipes/install.rb
+++ app/vm/cookbooks/magento_demo_builder/recipes/install.rb
@@ -4,8 +4,15 @@
 # frozen_string_literal: true
 
 data_pack_list = DataPackHelper.local_list
+build_action = node[:magento_demo_builder][:magento][:build][:action]
 
 unless data_pack_list.nil?
+	if %w[refresh].include?(build_action)
+		magento_cli 'Compiling dependencies after data pack creation' do
+			action :di_compile
+		end
+	end
+
 	data_pack_list.each do |data_pack|
 		magento_demo_builder 'Install data pack via the CLI' do
 			action :install
