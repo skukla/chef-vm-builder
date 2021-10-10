1d1fcb918915cd7c948801581658d977c64cff39
Steve Kukla <kukla@adobe.com>
2021-10-10 17:57:25 -0400

    Updated magento_demo_builder description

--- app/vm/cookbooks/magento_demo_builder/metadata.rb
+++ app/vm/cookbooks/magento_demo_builder/metadata.rb
@@ -2,15 +2,15 @@ name 'magento_demo_builder'
 maintainer 'Steve Kukla'
 maintainer_email 'kukla@adobe.com'
 license 'All Rights Reserved'
-description 'Installs/Configures composer'
+description 'Installs/Configures custom demo data'
 long_description 'Installs/Configures custom demo data'
 version '0.1.0'
 chef_version '>= 14.0'
 
-depends 'helpers'               # Brings in the various helpers
-depends 'nginx'                 # Brings in web root
-depends 'magento'               # Brings in Magento settings
-depends 'magento_patches'       # Brings in Magento patch settings
+depends 'helpers' # Brings in the various helpers
+depends 'nginx' # Brings in web root
+depends 'magento' # Brings in Magento settings
+depends 'magento_patches' # Brings in Magento patch settings
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
