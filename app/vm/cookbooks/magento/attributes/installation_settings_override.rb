1ec33a87ea561a570a354b404a863e9d9940ed69
Steve Kukla <kukla@adobe.com>
2021-10-25 00:10:07 -0400

    Removed erroneous error checking

--- app/vm/cookbooks/magento/attributes/installation_settings_override.rb
+++ app/vm/cookbooks/magento/attributes/installation_settings_override.rb
@@ -1,18 +1,17 @@
 # Cookbook:: magento
 # Attribute:: installation_settings_override
 # Copyright:: 2020, Steve Kukla, All Rights Reserved.
-#
 # Supported settings: backend_frontname language currency admin_firstname admin_lastname
-# admin_email admin_user admin_password unsecure_base_url secure_base_url use_rewrites
-# use_secure_frontend use_secure_admin cleanup_database session_save encryption_key
-#
+# admin_email admin_user admin_password use_secure_frontend use_secure_admin encryption_key
 # frozen_string_literal: true
 
 setting = ConfigHelper.value('application/settings')
 base_url = DemoStructureHelper.base_url
 
 unless setting.nil?
-	setting.each { |key, value| override[:magento][:settings][key] = value }
+	setting.each do |key, value|
+		override[:magento][:settings][key] = ValueHelper.bool_to_int(value)
+	end
 end
 unless base_url.empty?
 	override[:magento][:settings][:admin_email] = "admin@#{base_url}"
