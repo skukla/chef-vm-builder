5781dac386c29f1d7ff6e14776102a559f3b3faf
Steve Kukla <kukla@adobe.com>
2023-04-06 15:34:07 -0400

    Updated ARM base box

--- app/core/lib/hypervisor.rb
+++ app/core/lib/hypervisor.rb
@@ -2,57 +2,57 @@ require_relative 'config'
 require_relative 'guest_machine'
 
 class Hypervisor
-	def Hypervisor.value
-		Config.hypervisor
-	end
+  def Hypervisor.value
+    Config.hypervisor
+  end
 
-	def Hypervisor.list
-		%w[virtualbox vmware_fusion]
-	end
+  def Hypervisor.list
+    %w[virtualbox vmware_fusion]
+  end
 
-	def Hypervisor.plugins
-		case
-		when value.include?('virtualbox')
-		when value.include?('vmware')
-			%w[vagrant-vmware-desktop]
-		end
-	end
+  def Hypervisor.plugins
+    case
+    when value.include?('virtualbox')
+    when value.include?('vmware')
+      %w[vagrant-vmware-desktop]
+    end
+  end
 
-	def Hypervisor.gui
-		return Config.gui unless Config.gui.nil?
+  def Hypervisor.gui
+    return Config.gui unless Config.gui.nil?
 
-		case
-		when value.include?('virtualbox')
-			false
-		when value.include?('vmware')
-			true
-		end
-	end
+    case
+    when value.include?('virtualbox')
+      false
+    when value.include?('vmware')
+      true
+    end
+  end
 
-	def Hypervisor.base_box
-		boxes = {
-			'intel' => 'bento/ubuntu-22.04',
-			'm1' => 'bytesguy/ubuntu-server-22.04-arm64',
-		}
+  def Hypervisor.base_box
+    boxes = {
+      'intel' => 'bento/ubuntu-22.04',
+      'm1' => 'hajowieland/ubuntu-jammy-arm',
+    }
 
-		return Config.base_box unless Config.base_box.nil?
+    return Config.base_box unless Config.base_box.nil?
 
-		base_box = boxes['intel'] if GuestMachine.is_intel?
-		base_box = boxes['m1'] unless GuestMachine.is_intel?
+    base_box = boxes['intel'] if GuestMachine.is_intel?
+    base_box = boxes['m1'] unless GuestMachine.is_intel?
 
-		base_box
-	end
+    base_box
+  end
 
-	def Hypervisor.elasticsearch_requested?
-		unless Config.elasticsearch_requested?.nil?
-			return Config.elasticsearch_requested?
-		end
+  def Hypervisor.elasticsearch_requested?
+    unless Config.elasticsearch_requested?.nil?
+      return Config.elasticsearch_requested?
+    end
 
-		case
-		when value.include?('virtualbox')
-			true
-		when value.include?('vmware')
-			false
-		end
-	end
+    case
+    when value.include?('virtualbox')
+      true
+    when value.include?('vmware')
+      false
+    end
+  end
 end
