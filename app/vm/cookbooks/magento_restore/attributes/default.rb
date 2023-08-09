8bf97ada8afc9072693670e56789cfa1578b1c06
Steve Kukla <kukla@adobe.com>
2023-08-09 07:33:49 -0400

    Switched default restore mode to merge

--- app/vm/cookbooks/magento_restore/attributes/default.rb
+++ app/vm/cookbooks/magento_restore/attributes/default.rb
@@ -4,9 +4,9 @@
 # frozen_string_literal: true
 
 default[:magento_restore][:restore_path] =
-	'/var/chef/cache/cookbooks/magento_restore/files/default'
+  '/var/chef/cache/cookbooks/magento_restore/files/default'
 default[:magento_restore][:holding_area] = '/var/www/backups'
-default[:magento_restore][:mode] = 'separate'
+default[:magento_restore][:mode] = 'merge'
 default[:magento_restore][:source] = ''
 default[:magento_restore][:version] = ''
 default[:magento_restore][:local_files][:zip_file] = ''
