95019a9164c86124f6eb515fb9aa591480fa387a
Steve Kukla <kukla@adobe.com>
2023-08-14 16:57:44 -0400

    Fixed issue with hostmanager aliases

--- app/core/handlers/hosts_handler.rb
+++ app/core/handlers/hosts_handler.rb
@@ -1,22 +1,21 @@
 require_relative '../lib/demo_structure'
 
 class HostsHandler
-	def HostsHandler.manage_hosts(config)
-		config.hostmanager.enabled = true
-		config.hostmanager.manage_host = true
-		config.hostmanager.manage_guest = false
-		config.hostmanager.ignore_private_ip = false
-		config.hostmanager.include_offline = false
-		config.hostmanager.aliases = DemoStructure.additional_urls
-		config.hostmanager.ip_resolver =
-			proc do |machine|
-				result = ''
-				machine
-					.communicate
-					.execute('hostname -I') do |type, data|
-						result << data.split(' ')[1] if type == :stdout
-					end
-				result
-			end
-	end
+  def HostsHandler.manage_hosts(config)
+    config.hostmanager.enabled = true
+    config.hostmanager.manage_host = true
+    config.hostmanager.manage_guest = false
+    config.hostmanager.ignore_private_ip = false
+    config.hostmanager.include_offline = false
+    config.hostmanager.ip_resolver =
+      proc do |machine|
+        result = ''
+        machine
+          .communicate
+          .execute('hostname -I') do |type, data|
+            result << data.split(' ')[1] if type == :stdout
+          end
+        result
+      end
+  end
 end
